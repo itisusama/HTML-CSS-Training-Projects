e67f3c274b3a8dc748dd44ab1cba0c249f10176d
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2023-05-11 06:04:56 -0700

    Update index.php

--- Ninth Project/index.php
+++ Ninth Project/index.php
@@ -1,1 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 009</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- A -->
+    <div class="container">
+        <header>
+            <div class="header">
+                <div class="img"></div>
+                <h2>This site is Awesome</h2>
+                <p>This Website is Awsome and very easy to use.</p>
+                <button class="btn"><a href="#">Sign Up</a></button>
+            </div>
+        </header>
 
+        <main>
+            <div class="main">
+                <div class="main-block1">
+                    <div class="main-block1-items"></div>
+                    <div class="main-block1-items"></div>
+                    <div class="main-block1-items"></div>
+                    <div class="main-block1-items"></div>
+                </div>
+                
+                <div class="main-block2">
+                    <p class="main-block2-p">
+                        The insight and experience of successful leaders and
+                        entrepreneurs are valuable sources of inspiration and 
+                        motivation. The insight and experience of successful leaders and
+                        entrepreneurs are valuable sources of inspiration and 
+                        motivation. 
+                    </p><!-- Closing of .main-block2 -->
+                
+                <div class="main-block3">
+                    <div class="signup">
+                        <button class="btn2">sign up</button>
+                        <h3 class="heading">visit for more</h3>
+                        <p class="main-block3-p">for conecting us sign up for free,</p>
+                    </div>
+                </div><!-- Closing of .main-block3 -->
+            </div> <!-- Closing of .main -->
+        </main>
+        
+        <footer>
+            <div class="footer">Contact for Details</div>
+        </footer>
+    </div>
+</body>
+</html>
