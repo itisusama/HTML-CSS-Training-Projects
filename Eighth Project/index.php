a1e2b85f2c5ed8733f399b5de8fd1edc1edbb650
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2023-05-11 05:54:09 -0700

    Update index.php

--- Eighth Project/index.php
+++ Eighth Project/index.php
@@ -1,1 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 0008</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!--A-->
+    <div class="container">
+        <header>
+            <div class="header">
+                <div class="logo"><img src="download.jpg" alt="Logo"></div>
 
+                <ul class="list">
+                    <li>Site Menu Item 1</li>
+                    <li>Site Menu Item 2</li>
+                    <li>Site Menu Item 3</li>
+                    <li>Site Menu Item 4</li>
+                </ul>
+            </div>
+        </header>
+    <!--B-->   
+    <main>
+        <div class="main">
+            <div class="sidebar">
+                <div class="sidebar1">Sidebar Pagelet 1</div>
+                <div class="sidebar2">Sidebar Pagelet 2</div>
+                <div class="sidebar3">Sidebar Pagelet 3</div>
+            </div>
+
+            <div class="content"><h3>Main Content</h3></div>
+
+            <div class="index">
+                <ul class="index-list">
+                    <li>index item 1</li>
+                    <li>index item 2</li>
+                    <li>index item 3</li>
+                    <li>index item 4</li>
+                    <li>index item 5</li>
+                </ul>
+            </div><!--Closing of .index-->
+        </div><!--Closing of .main-->
+    </main>
+    <!--C-->
+    <footer>
+        <div class="footer"><b>Footer</b></div>
+    </footer>
+    </div>
+</body>
+</html>
